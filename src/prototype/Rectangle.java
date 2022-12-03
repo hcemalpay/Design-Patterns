f902d449671fd5c32a0b6cca121217c5393c099a
cem alpay <hcalpay@fonetyazilim.com>
2022-12-03 21:37:55 +0300

    Design Patterns

---
+++ src/prototype/Rectangle.java
@@ -0,0 +1,16 @@
+package prototype;
+
+/**
+ * @author hcalpay
+ */
+public class Rectangle extends Shape {
+
+    public Rectangle(){
+        type = "Rectangle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
---
+++ src/prototype/Square.java
@@ -0,0 +1,16 @@
+package prototype;
+
+/**
+ * @author hcalpay
+ */
+public class Square extends Shape {
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
